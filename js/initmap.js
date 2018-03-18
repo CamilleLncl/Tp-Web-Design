e7d1cb4abb22e9d04a26dd6d40342a8f71b35a37
CamilleLncln <camille.lancelin@epfedu.fr>
2018-03-18 12:13:34 +0100

    map ok

---
+++ js/initmap.js
@@ -0,0 +1,42 @@
+map.on('load', function () {
+
+	 map.addSource('mapCafe', {
+          type: 'geojson',
+          data: './data/mapCafe.geojson',
+          buffer: 0,
+          maxzoom: 12
+    });
+
+
+     map.addLayer({
+        'id': 'cafes',
+          'type': 'circle',
+          'source': 'mapCafe',
+        'paint': {
+          'circle-color': '#BF283C'
+      } }, 'waterway-label'); 
+ 
+
+     map.on('click', function(e) {
+  var features = map.queryRenderedFeatures(e.point, { layers: ['cafes'] });
+
+  var feature = features[0];
+
+  // Populate the popup and set its coordinates
+  // based on the feature found
+  var popup = new mapboxgl.Popup()
+  .setLngLat(feature.geometry.coordinates).addTo(map)
+  .setHTML(
+  '</br><b> NOM </b>: ' + feature.properties.nom +
+  '<b> ADRESSE </b>: ' + feature.properties.adresse + 
+  '</br><b> ARRONDISSEMENT </b>: ' + feature.properties.arrondissement +
+ 
+  '</br><b> PRIX</b>: ' + feature.properties.prix_comptoir )
+  .addTo(map);
+});
+
+     map.on('mousemove', function(e) {
+  var features = map.queryRenderedFeatures(e.point, { layers: ['cafes'] });
+  map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+});
+   });
--- js/script.js
+++ js/script.js
@@ -36,44 +36,4 @@
 	
 	var _this=this;
 })();
-map.on('load', function () {
-	 map.addSource('mapCafe', {
-          type: 'geojson',
-          data: './data/mapCafe.geojson',
-          buffer: 0,
-          maxzoom: 12
-    });
-
-
-     map.addLayer({
-        'id': 'cafes',
-          'type': 'circle',
-          'source': 'mapCafe',
-        'paint': {
-          'circle-color': '#fbb03b'
-      } }, 'waterway-label'); 
-
-     map.on('click', function(e) {
-  var features = map.queryRenderedFeatures(e.point, { layers: ['cafes'] });
-
-  var feature = features[0];
-
-  // Populate the popup and set its coordinates
-  // based on the feature found
-  var popup = new mapboxgl.Popup()
-  .setLngLat(feature.geometry.coordinates)
-  .setHTML(
-  	 '</br><b> NOM </b>: ' + feature.properties.fields.nom_du_cafe +
-  '<b> ADRESSE </b>: ' + feature.properties.fields.adresse + 
-  '</br><b> ARRONDISSEMENT </b>: ' + feature.properties.fields.arrondissement +
- 
-  '</br><b> PRIX</b>: ' + feature.properties.fields.prix_comptoir )
-  .addTo(map);
-});
-
-     map.on('mousemove', function(e) {
-  var features = map.queryRenderedFeatures(e.point, { layers: ['cafes'] });
-  map.getCanvas().style.cursor = features.length ? 'pointer' : '';
-});
-   });
 
